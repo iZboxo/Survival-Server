598c9340c70939056d2cbd308b06120571228042
iZboxo <zdenda.havlat@gmail.com>
2022-07-08 14:33:22 +0000

    auto-save

---
+++ git.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+NC='\033[0m'
+timestamp=$(date +%d-%m-%Y_%H-%M-%S)
+
+while true
+do
+git add .
+git commit -m "auto-save"
+git push
+printf "I ${RED}Saved the world${NC}  " 
+echo $timestamp
+
+sleep 60
+done
